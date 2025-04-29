b2a045acd3802a63886b971e0b718077e7af7a24
User <user@example.com>
2025-04-29 19:06:02 +0530

    Add environment variables configuration

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -7,15 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAXj8Z_6zK63HzbT2zhmdEoO3rYwq7ZwLg",
-  authDomain: "cool-7955f.firebaseapp.com",
-  projectId: "cool-7955f",
-  storageBucket: "cool-7955f.appspot.com",
-  messagingSenderId: "352883500489",
-  appId: "1:352883500489:web:3276b4d4cf50172527b418",
-  measurementId: "G-7E7ERF9MNZ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
   // Enable Firestore database
-  databaseURL: "https://cool-7955f-default-rtdb.firebaseio.com"
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
 // Initialize Firebase
